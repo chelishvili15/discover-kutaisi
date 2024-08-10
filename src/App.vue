48d73ccf70ca38e3bf9d0a8a8504eb8ddf562fbb
chelishvili <d.chelishvili91@gmail.com>
2024-08-10 12:37:25 +0400

    feat: add tours.json file

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,10 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import Card from './components/Card.vue'
+import Tour from '../public/tours.json'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="bg-gradient-to-br from-blue-600 via-cyan-500 to-green-200 w-screen min-h-screen p-2 flex flex-col gap-2">
+    <Card v-for="tour in Tour.data" :tour="tour" />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,5 +1,32 @@
 <template>
-    <div>
-        
+    <div class="bg-yellow-400 w-full py-3 px-3 rounded-md">
+        <div class="images flex justify-around mb-10">
+            <div class="img1 w-24 h-20 -rotate-12 rounded overflow-hidden">
+                <img :src="tour.locations[0].image" class="w-full h-full" :alt="tour.locations[0].name">
+            </div>
+            <div class="img2 w-24 h-20 rotate-6">
+                <img :src="tour.locations[1].image" class="w-full h-full" :alt="tour.locations[1].name">
+            </div>
+            <div v-if="tour.locations[2]" class="img3 w-24 h-20 -rotate-6">
+                <img :src="tour.locations[2].image" class="w-full h-full" :alt="tour.locations[2].name">
+            </div>
+        </div>
+
+        <div class="flex justify-between">
+            <div>
+                <p>Duration: {{ tour.duration }}</p>
+                <p>Type: {{ tour.type }}</p>
+                <p>Start and Finish: {{ tour.startFinish }}</p>
+                <p></p>
+                <p></p>
+            </div>
+            <div>
+                <p class="text-2xl font-semibold">PRICE: {{ tour.price }}</p>
+            </div>
+        </div>
     </div>
 </template>
+
+<script setup>
+defineProps(["tour"])
+</script>
