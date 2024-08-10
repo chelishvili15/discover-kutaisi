871174d0bc517c9a2f082dc43f1699ad9e1c6c4e
chelishvili <d.chelishvili91@gmail.com>
2024-08-10 18:49:53 +0400

    chore: change some design

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,18 +1,13 @@
 <template>
     <div class="bg-yellow-400 w-full py-3 px-3 rounded-md">
-        <div class="images flex justify-around mb-10">
-            <div class="img1 w-24 h-20 -rotate-12 rounded overflow-hidden">
-                <img :src="tour.locations[0].image" class="w-full h-full" :alt="tour.locations[0].name">
-            </div>
-            <div class="img2 w-24 h-20 rotate-6">
-                <img :src="tour.locations[1].image" class="w-full h-full" :alt="tour.locations[1].name">
-            </div>
-            <div v-if="tour.locations[2]" class="img3 w-24 h-20 -rotate-6">
-                <img :src="tour.locations[2].image" class="w-full h-full" :alt="tour.locations[2].name">
+        <div class="images flex justify-around mb-3">
+            <div v-for="(location, index) in tour.locations" class="img1 w-24 h-28 rounded overflow-hidden flex flex-col items-center" :class="index == 0 ? '-rotate-12' : index == 1 ? 'rotate-3' : 'rotate-6'">
+                <img :src="location.image" class="w-full h-20" :alt="location.name">
+                <p class="text-gray-700">{{ location.name }}</p>
             </div>
         </div>
 
-        <div class="flex justify-between">
+        <div class="flex justify-between text-gray-500">
             <div>
                 <p>Duration: {{ tour.duration }}</p>
                 <p>Type: {{ tour.type }}</p>
@@ -21,7 +16,7 @@
                 <p></p>
             </div>
             <div>
-                <p class="text-2xl font-semibold">PRICE: {{ tour.price }}</p>
+                <p class="text-2xl font-semibold text-gray-700">PRICE: {{ tour.price }}</p>
             </div>
         </div>
     </div>
