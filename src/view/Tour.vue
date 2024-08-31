48a60d30c79d46efc12d225770dcfb49c0fada76
chelishvili <d.chelishvili91@gmail.com>
2024-08-31 16:02:59 +0400

    feat: switch tabs on article

--- src/view/Tour.vue
+++ src/view/Tour.vue
@@ -1,10 +1,22 @@
 <template>
-    <div v-for="location in tour.locations">
-        <Article :location="location" />
-    </div>
+    <div class="flex my-8">
+        <div 
+            v-for="location in tour.locations" 
+            class="w-1/3 flex justify-center py-2"
+            :class="currentArticle.name == location.name ? 'border-l border-t border-r font-bold border-black' : 'border border-b-black'"
+            @click = "currentArticle = location"
+        >
+            <p>{{ location.name }}</p>
+        </div>
+
+    </div> 
+
+    <Article :location="currentArticle" />
+
 </template>
 
 <script setup>
+import { ref } from 'vue'
 import Article from '../components/Article.vue'
 import { useRoute, useRouter } from 'vue-router'
 import tours from '../tours.json'
@@ -14,5 +26,7 @@ const router = useRouter()
 
 const id = route.params.id
 const [tour] = tours.data.filter(ob => ob.id == id)
+
+const currentArticle = ref(tour.locations[0])
 
 </script>
