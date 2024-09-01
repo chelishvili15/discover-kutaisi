f3a68086e2038005777c9d04f77944f88dca64e1
chelishvili <d.chelishvili91@gmail.com>
2024-09-01 13:07:33 +0400

    feat: contact information

--- src/view/Contact.vue
+++ src/view/Contact.vue
@@ -1,5 +1,15 @@
 <template>
-    <div>
-        contact
+    <div class="absolute transform top-1/4 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <div class="flex items-center gap-4">
+            <Gmail />  <p>d.chelishvili91@gmail.com </p>
+        </div>
+        <div class="flex items-center gap-4">
+            <Whatsapp /> <p>+995 557 66 91 79</p>
+        </div>
     </div>
 </template>
+
+<script setup>
+import Gmail from '../assets/icons/Gmail.vue'
+import Whatsapp from '../assets/icons/Whatsapp.vue'
+</script>
