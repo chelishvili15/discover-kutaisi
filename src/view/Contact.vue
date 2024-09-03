0ea07f5d48518d7442cdc0ff4fb6c4f0ecc8b60d
chelishvili <d.chelishvili91@gmail.com>
2024-09-03 15:41:14 +0400

    chore: send message in gmail when click email

--- src/view/Contact.vue
+++ src/view/Contact.vue
@@ -1,8 +1,12 @@
 <template>
-    <div class="absolute transform top-1/4 left-1/2 -translate-x-1/2 -translate-y-1/2">
-        <div class="flex items-center gap-4">
-            <Gmail />  <p>d.chelishvili91@gmail.com </p>
-        </div>
+    <div class="absolute transform top-1/4 left-1/2 -translate-x-1/2 -translate-y-1/2"> 
+        <a
+            href="mailto:d.chelishvili91@gmail.com" 
+            class="flex items-center gap-4"
+        >
+            <Gmail />  
+            <p class="underline">d.chelishvili91@gmail.com </p>
+        </a>
         <a 
             href="https://api.whatsapp.com/send?phone=+995557669179" 
             class="flex items-center gap-4"
