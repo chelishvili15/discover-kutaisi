0e4edab25ff627779bad616aca4cc55c438d4050
chelishvili <d.chelishvili91@gmail.com>
2024-09-05 11:48:57 +0400

    refactor: transitons on routes

--- src/App.vue
+++ src/App.vue
@@ -7,11 +7,31 @@ const togSidebar = ref(false)
 </script>
 
 <template>
-    <div class="max-w-[768px] m-auto bg-transparent">
+    <div class="max-w-[768px] m-auto bg-transparent font-main">
       <Sidebar :toggleSidebar="togSidebar" @closeSidebar = "togSidebar = false" />
       <Header @toggleSidebar="togSidebar = !togSidebar" @closeSidebar="togSidebar = false" />
       <div class="bg-gradient-to-br w-full p-2 font-main" @click="togSidebar = false">
-        <router-view></router-view>
+        <router-view v-slot="{ Component }">
+          <transition name="slide-fade-route">
+            <component :is="Component" />
+          </transition>
+        </router-view>
       </div>
     </div>
 </template>
+
+<style>
+.slide-fade-route-enter-active {
+  transition: all 0.5s ease-out;
+}
+
+.slide-fade-route-leave-active {
+  transition: all 0s ease-in
+  
+}
+
+.slide-fade-route-enter-from,
+.slide-fade-route-leave-to {
+  transform: translateY(100%)
+}
+</style>
--- src/view/Contact.vue
+++ src/view/Contact.vue
@@ -1,19 +1,21 @@
 <template>
-    <div class="absolute transform top-1/4 left-1/2 -translate-x-1/2 -translate-y-1/2"> 
-        <a
-            href="mailto:d.chelishvili91@gmail.com" 
-            class="flex items-center gap-4"
-        >
-            <Gmail />  
-            <p class="underline">d.chelishvili91@gmail.com </p>
-        </a>
-        <a 
-            href="https://api.whatsapp.com/send?phone=+995557669179" 
-            class="flex items-center gap-4"
-        >
-            <Whatsapp /> 
-            <p class="underline">+995 557 66 91 79</p>
-        </a>
+    <div class="w-full h-[calc(100vh-200px)] flex justify-center pt-24"> 
+        <div class="w-min h-min">
+            <a
+                href="mailto:d.chelishvili91@gmail.com" 
+                class="flex items-center gap-4"
+            >
+                <Gmail />  
+                <p class="underline">d.chelishvili91@gmail.com </p>
+            </a>
+            <a 
+                href="https://api.whatsapp.com/send?phone=+995557669179" 
+                class="flex items-center gap-4"
+            >
+                <Whatsapp /> 
+                <p class="underline">+995 557 66 91 79</p>
+            </a>
+        </div>
     </div>
 </template>
 
