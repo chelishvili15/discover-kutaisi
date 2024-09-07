51da632e10e1d0fca004e3fd7147aaccd4a5d8c8
chelishvili <d.chelishvili91@gmail.com>
2024-09-07 15:05:08 +0400

    feat: add transffers route and transffers json

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -2,13 +2,14 @@
     <Transition name="slide-fade">
         <div 
             v-if="toggleSidebar"
-            class="fixed z-40 w-full h-screen text-gray-100 flex"
+            class="fixed z-40 w-full h-screen text-gray-100 flex text-2xl"
         >
             <div class="nav flex flex-col items-center justify-center shadow-lg gap-3 w-1/2 bg-green-600 h-full" @click="$emit('closeSidebar')">
-                <RouterLink to="/"   :class="route.path == `/` && 'font-bold'">Home</RouterLink>
-                <RouterLink to="/tours"   :class="route.path == `/tours` && 'font-bold'">Tours</RouterLink>
-                <RouterLink to="/about"   :class="route.path == `/about` && 'font-bold'">About</RouterLink>
-                <RouterLink to="/contact" :class="route.path == `/contact` && 'font-bold'">Contact</RouterLink>
+                <RouterLink to="/"   :class="route.path == `/` && 'font-bold text-white'">Home</RouterLink>
+                <RouterLink to="/tours"   :class="route.path == `/tours` && 'font-bold text-white'">Tours</RouterLink>
+                <RouterLink to="/transffers"   :class="route.path == `/transffers` && 'font-bold text-white'">Transffers</RouterLink>
+                <RouterLink to="/about"   :class="route.path == `/about` && 'font-bold text-white'">About</RouterLink>
+                <RouterLink to="/contact" :class="route.path == `/contact` && 'font-bold text-white'">Contact</RouterLink>
             </div>
             <div class="bg-transparent h-screen w-1/2" @click="$emit('closeSidebar')">
 
---
+++ src/view/Transffers.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { data } from './../transffers.json'
+</script>
+
+<template>
+    <div>
+        <div 
+            v-for="location in data" 
+            :key="location.id"
+            class="bg-green-600 rounded-lg text-gray-100 text-2xl font-bold px-7 py-3 mb-3 flex"
+        >
+            <div class="w-1/2">{{ location.name }}</div>
+            <div class="w-1/2 text-end">{{ location.price }}</div>
+        </div>
+    </div>
+</template>
