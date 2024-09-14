b87af571ffc737bb9231fb172381548939a22c1a
chelishvili <d.chelishvili91@gmail.com>
2024-09-14 19:47:32 +0400

    feat: home page with animation

--- src/view/Home.vue
+++ src/view/Home.vue
@@ -1,8 +1,104 @@
 <script setup>
+import BlackLogo from '../assets/icons/BlackLogo.vue'
 </script>
 
 <template>
-    <div>    
-       HOME
+    <BlackLogo class="opacity-5 w-full fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" /> 
+    
+
+    <div class="w-full h-[calc(100vh-200px)] flex justify-center pt-24">
+
+        <div class="w-full h-full flex flex-col justify-center gap-5 text-green-700">
+            <a 
+                href="#/tours" 
+                class="pot1 flex items-center gap-4"
+            >
+                <p class="underline">Tours</p>
+            </a>
+            <a 
+                href="#/transfers" 
+                class="pot2 flex items-center gap-4"
+            >
+                <p class="underline">Transfers</p>
+            </a>
+            <a 
+                href="#/about" 
+                class="pot3 flex items-center gap-4"
+            >
+                <p class="underline">About</p>
+            </a>
+            <a 
+                href="#/contact" 
+                class="pot4 flex items-center gap-4"
+            >
+                <p class="underline">Contact</p>
+            </a>
+        </div>
     </div>
+
+
 </template>
+
+<style>
+.pot1 {
+  top: 15%;
+  left: 50%;
+  transform: translateX(-50%);
+  position: absolute;
+  animation: linear;
+  animation-name: runLeft;
+  animation-duration: 3s;
+}
+.pot4 {
+  top: 60%;
+  left: 50%;
+  transform: translateX(-50%);
+  position: absolute;
+  animation: linear;
+  animation-name: runLeft;
+  animation-duration: 2s;
+}
+
+@keyframes runLeft {
+  0% {
+    left: -100%;
+  }
+  50% { 
+    left: 10%; 
+   }
+  100% {
+    left: 50%;     
+  }
+}
+
+.pot2 {
+  top: 30%;
+  right: 50%;
+  transform: translateX(50%);
+  position: absolute;
+  animation: linear;
+  animation-name: runRight;
+  animation-duration: 2s;
+}
+.pot3 {
+  top: 45%;
+  right: 50%;
+  transform: translateX(50%);
+  position: absolute;
+  animation: linear;
+  animation-name: runRight;
+  animation-duration: 3s;
+}
+
+@keyframes runRight {
+  0% {
+    right: -100%;
+  }
+  50% { 
+    right: 10%; 
+   }
+  100% {
+    right: 50%;     
+  }
+}
+</style>
