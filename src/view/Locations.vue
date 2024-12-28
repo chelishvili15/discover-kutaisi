6c15ce0e2fcb7edbfcfd2ebcf7b5a7f44d8e772b
chelishvili15 <d.chelishvili15@gmail.com>
2024-12-28 14:16:05 +0400

    feat: add header for seo

--- src/view/Locations.vue
+++ src/view/Locations.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="flex flex-col gap-4">
+        <h1 class="text-2xl font-bold m-auto my-7">Places to visit Kutaisi</h1>
         <div v-for="location in locations">
             <LocationsCard :location="location" @click="router.push(`/locations/${location.id}`)" />
         </div>
