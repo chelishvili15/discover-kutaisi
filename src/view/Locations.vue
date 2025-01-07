8550e357436564ebf6b387df9865fbd1dfe61f85
chelishvili15 <d.chelisvhili91@gmail.com>
2025-01-07 23:29:48 +0400

    chore: use computed in locations

--- src/view/Locations.vue
+++ src/view/Locations.vue
@@ -2,7 +2,7 @@
     <div class="flex flex-col gap-4 mb-10">
         <H1 title="Places to visit Kutaisi"></H1>
 
-        <div v-for="location in locations.slice(((route.params.page - 1) * itemsPerPage), ((route.params.page - 1)* itemsPerPage + itemsPerPage))">
+        <div v-for="location in slicedLocations">
             <LocationsCard
               :location="location"
               @click="router.push(`/places-to-visit-kutaisi/${location.id}`)" />
@@ -13,15 +13,21 @@
 </template>
 
 <script setup>
+import { computed, ref } from "vue";
+import { useRoute } from "vue-router"
 import locations from "../locations.json"
 import LocationsCard from "../components/LocationsCard.vue"
 import router from "../router";
 import H1 from "../components/H1.vue";
 import Pagination from "../components/Pagination.vue"
-import { useRoute } from "vue-router"
-import { ref } from "vue";
 
-const itemsPerPage = ref(3)
+const itemsPerPage = ref(5)
 
 const route = useRoute()
+
+const slicedLocations = computed(() => {
+    const first = (route.params.page - 1) * itemsPerPage.value
+    const last = (route.params.page - 1) * itemsPerPage.value + itemsPerPage.value
+    return locations.slice(first, last)
+})
 </script>
