c03b0c4f5071f23cb5879246e9d141ef72793aa8
chelishvili15 <d.chelisvhili91@gmail.com>
2025-02-17 15:41:08 +0400

    chore: remove imported vercel package

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
 <script setup>
-import { Analytics } from '@vercel/analytics/vue'
 import { ref } from 'vue'
 import Header from './components/Header.vue'
 import Footer from './components/Footer.vue'
@@ -10,7 +9,6 @@ const togSidebar = ref(false)
 </script>
 
 <template>
-  <Analytics />
   <BlackLogo class="opacity-5 w-full fixed -z-50 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" /> 
     <div class="max-w-[768px] m-auto bg-transparent font-main relative">
       <Sidebar :toggleSidebar="togSidebar" @closeSidebar = "togSidebar = false" />
